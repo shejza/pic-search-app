8923fa0b33d56e6be6a007a36063954654e1de45
LAPTOP-KN35FHQO\Shejz <shejzaberisha@gmail.com>
2019-05-28 22:06:32 +0200

    add state

--- src/index.js
+++ src/index.js
@@ -1,38 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import SeasonDisplay from './SeasonDisplay';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
 
-
-class App extends React.Component {
-
-    state = {
-        latitude:null,
-        errorMessage:''
-    };
-
-    componentDidMount() {
-        
-        window.navigator.geolocation.getCurrentPosition(
-            position => this.setState({ latitude: position.coords.latitude}),
-            err =>  this.setState({ errorMessage: err.message})
-        );
-    }
-
-    render() {
-     
-      
-            if(this.state.errorMessage && !this.state.latitude) {
-return <div> Error {this.state.errorMessage}</div>;
-            } 
-            if(!this.state.errorMessage && this.state.latitude) {
-                return <SeasonDisplay latitude = {this.state.latitude}/>
-     
-            }
-
-            return <div>Loading...</div>;
-    }
-}
-
-ReactDOM.render(
-    <App/>, document.querySelector('#root')
-);
+ReactDOM.render(<App />, document.querySelector("#root"));
